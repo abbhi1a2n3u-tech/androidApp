b8c6996c736e1b9df8c417fbbe19a5c4fdc5d5c7
abbhi1a2n3u-tech <abbhi1a2n3u@gmail.com>
2025-10-24 13:24:23 +0530

    Server update 2.5.0

---
+++ DB/db.js
@@ -0,0 +1,5 @@
+import postgres from 'postgres'
+
+const sql = postgres("postgresql://postgres:8535011326@db.kkymczgouhlhxbrpoqri.supabase.co:5432/postgres")
+
+export default sql
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import e from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import sql from "./DB/db.js";
 
 
 const app = e();
@@ -8,9 +9,19 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
+
+
 app.get("/", (req, res) => {
     res.send("Hello from Backend!");
 });
+
+const users = await sql`SELECT * FROM users;`;
+
+console.log(users);
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
