f6da3173c7c36e80ce17a78779ae9bc854fa341e
abbhi1a2n3u-tech <abbhi1a2n3u@gmail.com>
2025-10-24 13:07:13 +0530

    Server update 2.4.7

--- Routes/router.js
+++
@@ -1,8 +0,0 @@
-import e from "express";
-import { register } from "../APIs/keyAuth.js";
-
-const router = e.Router();
-
-router.post("/register", register);
-
-export { router };
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-// index.js
-// import mongoose from "mongoose";
-// import { User } from "./model/user.js";
+import e from "express ";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-import "./server.js";
 
+const app = e();
+const PORT = 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from Backend!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
