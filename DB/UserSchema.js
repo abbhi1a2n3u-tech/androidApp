e21c82a5396510050ed94421ce6479504d77cf42
abbhi1a2n3u-tech <abbhi1a2n3u@gmail.com>
2025-10-24 15:33:31 +0530

    Server update 2.7.0

--- DB/UserSchema.js
+++ DB/UserSchema.js
@@ -4,7 +4,7 @@ import connectDB from "./db.js";
 const conn = await connectDB();
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
